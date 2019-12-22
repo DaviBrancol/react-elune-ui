1d291e8fc9b3f3676337bc02c36e2d7507fb5768
DaviBrancol <daviduarbrancol@gmail.com>
2019-12-22 04:56:17 -0300

    Added initial color scheme purple-blue to project css hierarchy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './css/App.css'
+import './css/purple-blue.css'
 
 export default function App(props) {
-  return <div className="application"></div>
+  return <div className="application purple-blue"></div>
 }
